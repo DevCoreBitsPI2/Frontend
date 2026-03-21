acad993a58e86b16bfab0675d4f6a3d754b8adc5
Juan Zuluaga <104238155+Romerito1125@users.noreply.github.com>
2026-03-20 19:24:36 -0500

    Remove comments from cargo interfaces
    Removed comments from CargoConArea and CargoCompleto interfaces.

--- types/cargo.ts
+++ types/cargo.ts
@@ -6,12 +6,10 @@ export interface CargoBase {
   nombre: string;
 }
 
-// cargo con área simple
 export interface CargoConArea extends CargoBase {
   area: AreaBase;
 }
 
-// cargo completo (con sus propios administradores)
 export interface CargoCompleto extends CargoBase {
   area: AreaConAdministrador; // administrador del área
   administrador: AdministradorBase; // Administrador que creó el cargo
